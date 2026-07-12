a9ea9d773ea7a83b5fe797ee50077df6e4932a97
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-07-12 12:18:27 +0200

    chore: adding wortremover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     scala
     // Apply scalatest plugin to add scalatest support through `test` task.
     id("com.github.maiflai.scalatest") version "0.33"
+    //
+    id("io.github.jahrim.wartremover") version "0.1.3"
 }
 
 tasks.withType<ScalaCompile>().configureEach {
@@ -23,13 +25,13 @@ java {
 }
 
 dependencies {
-    val scalaVersion = "3.8.4"
+    val scala = "3.8.4"
     val (scalaMinor, _) = requireNotNull(
         Regex("^(\\d+)(\\.\\d+)(\\.\\d+)?$")
-            .matchEntire(scalaVersion)
+            .matchEntire(scala)
     )
         .destructured
-    implementation("org.scala-lang:scala3-library_$scalaMinor:$scalaVersion")
+    implementation("org.scala-lang:scala3-library_$scalaMinor:$scala")
     testImplementation("org.scalatest:scalatest_$scalaMinor:3.2.12")
     testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.64.8")
 }
@@ -40,3 +42,4 @@ tasks.withType<Test>().configureEach {
         showCauses = true
     }
 }
+
