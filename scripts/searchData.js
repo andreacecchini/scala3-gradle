8c5fb341ac49e515c3e69799a76c3ea9bf1b07f2
andreacecchini <andreacecchini@users.noreply.github.com>
2026-07-12 11:53:37 +0000

    deploy: 71ec94818111098124b6afc02e2ce263af1943f5

---
+++ scripts/searchData.js
@@ -0,0 +1,5 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"scala3-gradle API","t":"scala3-gradle API","d":"","k":"static","x":""},
+{"l":"org/example.html#","e":false,"i":"","n":"org.example","t":"org.example","d":"","k":"package","x":""},
+{"l":"org/example/Main$.html#","e":false,"i":"","n":"Main","t":"Main","d":"org.example","k":"object","x":""},
+{"l":"org/example/Main$.html#greeting-0","e":false,"i":"","n":"greeting","t":"greeting: String","d":"org.example.Main","k":"def","x":""},
+{"l":"org/example/Main$.html#main-913","e":false,"i":"","n":"main","t":"main(args: Array[String]): Unit","d":"org.example.Main","k":"def","x":""}];
